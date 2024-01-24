db96c6b748c9c85929bebee30b518ef06a5dd38c
ligiuitao <liguitao@yitong.com.cn>
2024-01-24 21:00:36 +0800

    ch02

---
+++ shelter_main/src/commands/hello.rs
@@ -0,0 +1,14 @@
+use clap::{ArgMatches, Command};
+
+pub fn configure() -> Command{
+    Command::new("hello").about("hello world!")
+}
+
+pub fn handle(matches:&ArgMatches)->anyhow::Result<()> {
+    if let Some(_matches) = matches.subcommand_matches("hello") {
+        println!("hello world!")
+    }
+
+    Ok(())
+
+}
--- shelter_main/src/main.rs
+++ shelter_main/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Arg, Command};
+use dotenv::dotenv;
+use shelter_main::commands;
+pub fn main() -> anyhow::Result<()>{
+    dotenv().ok();
+    let mut command = Command::new("Dog Shelter sample application")
+        .version("1.0")
+        .author("mrliguitao@163.com")
+        .about("A sample application to experiment with Rust-based microservices")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("Configuration file location" )
+                .default_value("config.json")
+        );
+    command = commands::configure(command);
+    let matches = command.get_matches();
+    commands::handle(&matches)?;
+    Ok(())
 }
