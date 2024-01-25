f318870c2a3a250875115a7eb2bd745ccf38b150
ligiuitao <liguitao@yitong.com.cn>
2024-01-25 14:10:10 +0800

    ch03

--- shelter_main/src/main.rs
+++ shelter_main/src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Arg, Command};
 use dotenv::dotenv;
 use shelter_main::commands;
+use shelter_main::settings;
 pub fn main() -> anyhow::Result<()>{
     dotenv().ok();
     let mut command = Command::new("Dog Shelter sample application")
@@ -16,6 +17,27 @@ pub fn main() -> anyhow::Result<()>{
         );
     command = commands::configure(command);
     let matches = command.get_matches();
-    commands::handle(&matches)?;
+    let config_location = matches
+        .get_one::<String>("config")
+        .map(|s|s.as_str())
+        .unwrap_or("");
+    let settings = settings::Settings::new(config_location,"SHELTER")?;
+    commands::handle(&matches,&settings)?;
+
+    println!(
+        "db url: {}",
+        settings
+            .database
+            .url
+            .unwrap_or("missing database url".to_string())
+    );
+    println!(
+        "logging level: {}",
+        settings
+            .logging
+            .log_level
+            .unwrap_or("info".to_string())
+    );
+
     Ok(())
 }
