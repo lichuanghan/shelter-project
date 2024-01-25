d06c972a48da8c03d8bd5b15fe6389b118120285
ligiuitao <liguitao@yitong.com.cn>
2024-01-25 14:10:10 +0800

    第三章

--- shelter_main/src/commands/hello.rs
+++ shelter_main/src/commands/hello.rs
@@ -1,10 +1,11 @@
 use clap::{ArgMatches, Command};
+use crate::settings::Settings;
 
 pub fn configure() -> Command{
     Command::new("hello").about("hello world!")
 }
 
-pub fn handle(matches:&ArgMatches)->anyhow::Result<()> {
+pub fn handle(matches:&ArgMatches, settings: &Settings) ->anyhow::Result<()> {
     if let Some(_matches) = matches.subcommand_matches("hello") {
         println!("hello world!")
     }
